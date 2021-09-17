ee836c195b786e279a1ab657fa0f0349d0fd9e32
毛小文 <neoway@126.com>
2021-09-17 21:20:28 +0800

    init

--- Core/Inc/mysetp.h
+++ Core/Inc/mysetp.h
@@ -3,5 +3,6 @@
 #include "main.h"
 #include "u8g.h"
 #include "u8g_com_stm32_hw_i2c_hal.h"
+
 void setp();
 #endif
--- Core/Src/mysetp.c
+++ Core/Src/mysetp.c
@@ -4,15 +4,17 @@ u8g_t u8g;
 void setp(){
     u8g_InitComFn(&u8g,&u8g_dev_ssd1306_128x64_i2c,u8g_com_hw_i2c_fn);
     HAL_Delay(11);
-    u8g_DrawLine(&u8g,10,10,40,40);
+
     while (1)
     {
        u8g_FirstPage(&u8g);
        do
        {
-          u8g_DrawLine(&u8g,10,10,40,40);
+     
+      u8g_DrawStr(&u8g, 2, 12, "Hello!");//write string - you set coordinates and string  
+      u8g_DrawBox(&u8g, 30, 30, 35, 35);//draw some box  
        } while (u8g_NextPage(&u8g));
-       
+       u8g_Delay(10);
     }
     
 }
