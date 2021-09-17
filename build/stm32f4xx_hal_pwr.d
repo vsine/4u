5c4453faba6589eae15af93db8607411fb51e48c
毛小文 <neoway@126.com>
2021-09-17 19:41:47 +0800

    init

--- build/stm32f4xx_hal_pwr.d
+++ build/stm32f4xx_hal_pwr.d
@@ -21,6 +21,8 @@ build/stm32f4xx_hal_pwr.o: \
  Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
  Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
  Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
  Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
  Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
 
@@ -69,6 +71,10 @@ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
 Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
 
 Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
 
 Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
 
--- u8glib/u8g_com_stm32_hw_i2c_hal.c
+++ u8glib/u8g_com_stm32_hw_i2c_hal.c
@@ -1,5 +1,6 @@
 #include "u8g.h"
-
+#include "main.h"
+extern I2C_HandleTypeDef hi2c1;
 
 
 void u8g_Delay(uint16_t val)
@@ -20,6 +21,9 @@ void u8g_10MicroDelay()
 
 uint8_t u8g_com_hw_i2c_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_ptr)
 {
+    static uint8_t buffer[128];
+    static uint8_t buf_idx;
+    uint8_t *data;
   switch(msg)
   {
     case U8G_COM_MSG_INIT:
@@ -27,15 +31,28 @@ uint8_t u8g_com_hw_i2c_fn(u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_pt
     case U8G_COM_MSG_STOP:
       break;
 
-   
+
     case U8G_COM_MSG_CHIP_SELECT:
       /* arg_val contains the chip number, which should be enabled */
+      if (arg_val==0)
+         buf_idx=0;
+      
+      
       break;
 
 
     case U8G_COM_MSG_WRITE_BYTE:
+        if (HAL_I2C_Master_Transmit(&hi2c1, (0x78), buffer, buf_idx,1000) != HAL_OK)
+               return 0;
       break;
     case U8G_COM_MSG_WRITE_SEQ:
+        data = (uint8_t *)arg_ptr;
+        while (arg_val > 0)
+        {
+            buffer[buf_idx++] = *data;
+            data++;
+            arg_val--;
+        }
       break;
   }
   return 1;
