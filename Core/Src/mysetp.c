07b48a6b92dad44d6eeebccc1d2afd6c7d32f148
毛小文 <neoway@126.com>
2021-09-17 22:18:22 +0800

    init

--- Core/Src/mysetp.c
+++ Core/Src/mysetp.c
@@ -1,12 +1,12 @@
 #include "mysetp.h"
 u8g2_t u8g2;
 void setp(){
- 
+    HAL_Delay(100);
     u8g2Init(&u8g2);
-    u8g2_SetFont(&u8g2, u8g2_font_6x13_tr);
+    u8g2_SetFont(&u8g2, u8g2_font_unifont_t_chinese2);
     while (1)
     {
-        u8g2_DrawStr(&u8g2,0,10,"Neotoxic");
+        u8g2_DrawStr(&u8g2,0,10,"Volt:10V");
         u8g2_DrawLine(&u8g2,10,60,118,60);
         u8g2_DrawLine(&u8g2,10,20,10,60);
         u8g2_SendBuffer(&u8g2);
--- u8g2_lib/u8g2_com_stm32_hw_i2c_hal.c
+++ u8g2_lib/u8g2_com_stm32_hw_i2c_hal.c
@@ -11,17 +11,10 @@ uint8_t u8x8_byte_hw_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_p
     switch (msg)
     {
     case U8X8_MSG_BYTE_INIT:
-    {
-        /* add your custom code to init i2c subsystem */
-       // MX_I2C1_Init(); //I2C初始化
-       // ihi2c=getHI2C1();
-    }
     break;
 
     case U8X8_MSG_BYTE_START_TRANSFER:
-    {
         buf_idx = 0;
-    }
     break;
 
     case U8X8_MSG_BYTE_SEND:
@@ -36,16 +29,9 @@ uint8_t u8x8_byte_hw_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_p
         }
     }
     break;
-
     case U8X8_MSG_BYTE_END_TRANSFER:
-    {
-
         if (HAL_I2C_Master_Transmit(&hi2c1, (OLED_ADDRESS), buffer, buf_idx,1000) != HAL_OK)
                return 0;
-        
-         
-        
-    }
     break;
 
     case U8X8_MSG_BYTE_SET_DC:
@@ -58,50 +44,39 @@ uint8_t u8x8_byte_hw_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_p
     return 1;
 }
 
-void delay_us(uint32_t time)
-{
-    uint32_t i = 8 * time;
-    while (i--)
-        ;
-}
+
 
 uint8_t u8x8_gpio_and_delay(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr)
 {
     switch (msg)
     {
     case U8X8_MSG_DELAY_100NANO: // delay arg_int * 100 nano seconds
-        __NOP();
         break;
     case U8X8_MSG_DELAY_10MICRO: // delay arg_int * 10 micro seconds
-        for (uint16_t n = 0; n < 320; n++)
-        {
-            __NOP();
-        }
         break;
     case U8X8_MSG_DELAY_MILLI: // delay arg_int * 1 milli second
         HAL_Delay(1);
         break;
     case U8X8_MSG_DELAY_I2C: // arg_int is the I2C speed in 100KHz, e.g. 4 = 400 KHz
-        delay_us(5);
         break;                    // arg_int=1: delay by 5us, arg_int = 4: delay by 1.25us
     case U8X8_MSG_GPIO_I2C_CLOCK: // arg_int=0: Output low at I2C clock pin
         break;                    // arg_int=1: Input dir with pullup high for I2C clock pin
     case U8X8_MSG_GPIO_I2C_DATA:  // arg_int=0: Output low at I2C data pin
         break;                    // arg_int=1: Input dir with pullup high for I2C data pin
     case U8X8_MSG_GPIO_MENU_SELECT:
-        u8x8_SetGPIOResult(u8x8, /* get menu select pin state */ 0);
+       // u8x8_SetGPIOResult(u8x8, /* get menu select pin state */ 0);
         break;
     case U8X8_MSG_GPIO_MENU_NEXT:
-        u8x8_SetGPIOResult(u8x8, /* get menu next pin state */ 0);
+        //u8x8_SetGPIOResult(u8x8, /* get menu next pin state */ 0);
         break;
     case U8X8_MSG_GPIO_MENU_PREV:
-        u8x8_SetGPIOResult(u8x8, /* get menu prev pin state */ 0);
+        //u8x8_SetGPIOResult(u8x8, /* get menu prev pin state */ 0);
         break;
     case U8X8_MSG_GPIO_MENU_HOME:
-        u8x8_SetGPIOResult(u8x8, /* get menu home pin state */ 0);
+        //u8x8_SetGPIOResult(u8x8, /* get menu home pin state */ 0);
         break;
     default:
-        u8x8_SetGPIOResult(u8x8, 1); // default return value
+        //u8x8_SetGPIOResult(u8x8, 1); // default return value
         break;
     }
     return 1;
